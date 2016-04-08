85b36c25b933cd0c2733a74733c507a987011277
AlanPolson <polson.alan@gmail.com>
2016-04-07 23:42:08 -0400

    added correct run.sh

--- final_functions/run.sh
+++ final_functions/run.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 MAPPER=$1
 REDUCER=$2
+MAPPER_NAME=$(basename $MAPPER)
+REDUCER_NAME=$(basename $REDUCER)
 INPUT=$3
 OUTPUT=$4
-LOCAL_OUTPUT=$5
+NUM_MAPPERS=$5
+NUM_REDUCERS=$6
+LOCAL_OUTPUT=$7
 STREAMING_JAR=/opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar
 hadoop fs -rm -r -skipTrash "${OUTPUT}"
 hadoop jar ${STREAMING_JAR} \
-    -D mapred.job.name='BDM_Hw5' \
-    -D mapred.map.tasks=2 \
+    -D mapred.job.name='BDM_HW5' \
+    -D mapred.map.tasks=${NUM_MAPPERS} \
     -files "${MAPPER}","${REDUCER}" \
-    -mapper "${MAPPER}" \
-    -reducer "${REDUCER}" \
+    -mapper "${MAPPER_NAME}" \
+    -reducer "${REDUCER_NAME}" \
     -input "${INPUT}" \
     -output "${OUTPUT}" \
-    -numReduceTasks 2
+    -numReduceTasks ${NUM_REDUCERS}
 
 if [ ! -z "${LOCAL_OUTPUT}" ]; then
     rm -f "${LOCAL_OUTPUT}"
